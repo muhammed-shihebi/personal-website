5d45f0d92770a7e386f0b114260104510449e763
Nour0700 <58932402+Nour0700@users.noreply.github.com>
2020-09-26 03:14:24 +0300

    init express server and adding new pages

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # hardcoded-personal-website
 
-This is my personal website, which is completely handcoded without using CMS
+This is my personal website, which is completely hand-coded without using CMS
---
+++ app.js
@@ -0,0 +1,32 @@
+// =========================================================== Init
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const ejs = require ("ejs"); 
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.listen(3000, function() {console.log("Server Started successfully");});
+
+// =========================================================== 
+
+app.get("/", function(req, res){
+	res.render("home")
+});
+app.get("/about-me", function(req, res){
+	res.render("about-me")
+});
+app.get("/projects", function(req, res){
+	res.render("projects")
+});
+app.get("/contact-me", function(req, res){
+	res.render("contact-me")
+});
+app.get("/blog", function(req, res){
+	res.render("blog")
+});
+
+app.get("/cv", function(req, res){
+	res.render("cv")
+})
