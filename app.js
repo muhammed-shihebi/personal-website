1b549fddcd52285059929688b535dfc6e792fc55
Nour0700 <58932402+Nour0700@users.noreply.github.com>
2020-09-27 16:55:21 +0300

    adding contentful api

--- app.js
+++ app.js
@@ -3,32 +3,60 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const ejs = require ("ejs"); 
+const ejs = require("ejs");
+const contentful = require('contentful');
+const richTextRender = require('@contentful/rich-text-html-renderer');
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
-app.listen(3000, function() {console.log("Server Started successfully");});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(3000, function () { console.log("Server Started successfully"); });
 
 // =========================================================== 
 
-app.get("/", function(req, res){
+var client = contentful.createClient({
+	space: 'su42bjsoz92f',
+	accessToken: 'v5DXJtoxn3T2jeR4ez8br7PQGFDTNKHlIBbabMFOfY0'
+});
+
+let blog = [];
+
+// ===========================================================
+
+app.get("/", function (req, res) {
 	res.render("home")
 });
-app.get("/about-me", function(req, res){
+app.get("/about-me", function (req, res) {
 	res.render("about-me")
 });
-app.get("/projects", function(req, res){
+app.get("/projects", function (req, res) {
 	res.render("cards")
 });
-app.get("/contact-me", function(req, res){
+app.get("/contact-me", function (req, res) {
 	res.render("contact-me")
 });
-app.get("/blog", function(req, res){
+app.get("/blog", function (req, res) {
+
+	blog = []
+	client.getEntries({ content_type: 'post' }).then(function (entries) {
+		entries.items.forEach(function (entry) {
+			if (entry.fields) {
+				const rawRichTextField = entry.fields.content;
+				const htmlContent = richTextRender.documentToHtmlString(rawRichTextField);
+				var post = {
+					title: entry.fields.title,
+					imageUrl: entry.fields.postImage.fields.file.url,
+					content: htmlContent
+				};
+				blog.push(post);
+			}
+		});
+		console.log(blog);
+	});
 	res.render("cards")
 });
-app.get("/cv", function(req, res){
+app.get("/cv", function (req, res) {
 	res.render("cv")
-}); 
-app.get("/post", function(req, res){
+});
+app.get("/post", function (req, res) {
 	res.render("post")
 }); 
